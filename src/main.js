4ce1acfa8dc1e9753b6070b768ab2cedb0f2a558
FabianXRL16 <fabianpacherres@gmail.com>
2022-08-05 19:16:38 -0500

    add fontAwesome

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faAddressCard } from '@fortawesome/free-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faAddressCard)
 Vue.config.productionTip = false
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
